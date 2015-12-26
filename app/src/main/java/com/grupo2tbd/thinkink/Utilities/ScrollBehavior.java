c446b0c826567dbf3b3d248770b65de64b1c536e
luis <luis.9993@gmail.com>
2015-12-26 15:33:33 -0300

    Ocultando la cosa

---
+++ app/src/main/java/com/grupo2tbd/thinkink/Utilities/ScrollBehavior.java
@@ -0,0 +1,31 @@
+package com.grupo2tbd.thinkink.Utilities;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by luis on 12/26/15.
+ */
+public class ScrollBehavior extends FloatingActionButton.Behavior {
+    public ScrollBehavior(Context context, AttributeSet attributeSet){
+        super();
+    }
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if(dyConsumed > 0 && child.getVisibility() == View.VISIBLE){
+            child.hide();
+        } else{
+            child.show();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+}
